882fabfb4a996376228f57aad6f852afaa47009d
jitarth <abc@gmail.com>
2021-06-10 16:54:32 +0530

    Commit 10/6/21 4:54 p m

--- lib/util/common_utils.dart
+++ lib/util/common_utils.dart
@@ -1,93 +1,9 @@
+import 'package:enum_to_string/enum_to_string.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
-
-Widget getLabel(var label) {
-  return Text(
-    label,
-    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-  );
-}
-
-class CustomeTextField extends StatefulWidget {
-  var hint;
-  var icon;
-  bool isObscure;
-  bool isIconNeeded;
-  TextInputType inputType;
-  TextEditingController controller;
-
-  CustomeTextField({this.hint,this.icon,this.isObscure=false,this.isIconNeeded=false,this.controller,this.inputType});
-  @override
-  _CustomeTextFieldState createState() => _CustomeTextFieldState();
-}
-
-class _CustomeTextFieldState extends State<CustomeTextField> {
-  @override
-  Widget build(BuildContext context) {
-    if(widget.inputType == TextInputType.number)
-      {
-        return Container(
-          decoration: BoxDecoration(border: Border.all(color: Colors.black)),
-          child: Padding(
-            padding: EdgeInsets.only(left: 5, right: 5),
-            child: Row(
-              children: [
-                if(widget.isIconNeeded == true)
-                  Icon(
-                    widget.icon,
-                    color: Color(0xFF0A5189),
-                  ),
-                SizedBox(
-                  width: 5,
-                ),
-                Expanded(
-                    child: TextFormField(
-                      keyboardType: widget.inputType,
-                      controller: widget.controller,
-                      obscureText: widget.isObscure,
-                      decoration: InputDecoration(
-                        hintText: widget.hint,
-                        border: InputBorder.none,
-                      ),
-                    ))
-              ],
-            ),
-          ),
-        );
-      }
-    else
-      {
-        return Container(
-          decoration: BoxDecoration(border: Border.all(color: Colors.black)),
-          child: Padding(
-            padding: EdgeInsets.only(left: 5, right: 5),
-            child: Row(
-              children: [
-                if(widget.isIconNeeded == true)
-                  Icon(
-                    widget.icon,
-                    color: Color(0xFF0A5189),
-                  ),
-                SizedBox(
-                  width: 5,
-                ),
-                Expanded(
-                    child: TextFormField(
-                      controller: widget.controller,
-                      obscureText: widget.isObscure,
-                      decoration: InputDecoration(
-                        hintText: widget.hint,
-                        border: InputBorder.none,
-                      ),
-                    ))
-              ],
-            ),
-          ),
-        );
-      }
-  }
-}
-
+import 'package:flutterdemo1/Providers/ProviderClass.dart';
+import 'package:flutterdemo1/util/Common.dart';
+import 'package:provider/provider.dart';
 void show(BuildContext context, String statement)
 {
   showDialog(
@@ -109,4 +25,5 @@ Widget closeDialog(BuildContext context) {
     child: Text("Ok"),
   );
 }
+
 
